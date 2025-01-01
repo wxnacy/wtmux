69600042307aed25b5d29be078dd6c0535bab529
wxnacy <wxnacy@gmail.com>
2025-01-01 17:22:18 +0800

    chore(gitmodule): add catppuccin shell

--- gitmodule.sh
+++ gitmodule.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 git submodule add --force https://github.com/tmux-plugins/tpm plugins/tpm
+git submodule add --force https://github.com/catppuccin/tmux.git plugins/catppuccin
 
 # ln -sf $(PWD) ${HOME}/.tmux
 # ln -sf $(PWD)/tmux.conf ${HOME}/.tmux.conf
