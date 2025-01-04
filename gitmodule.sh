dba798810d360f52da176e12376f11fb23309335
wxnacy <wxnacy@gmail.com>
2025-01-04 15:54:26 +0800

    feat(plugins): use tmux-fzf-url replace tmux-urlview

--- gitmodule.sh
+++ gitmodule.sh
@@ -2,6 +2,7 @@
 
 git submodule add --force https://github.com/tmux-plugins/tpm plugins/tpm
 git submodule add --force https://github.com/catppuccin/tmux.git plugins/catppuccin
+git submodule add --force https://github.com/wfxr/tmux-fzf-url plugins/fzf-url
 
 # ln -sf $(PWD) ${HOME}/.tmux
 # ln -sf $(PWD)/tmux.conf ${HOME}/.tmux.conf
